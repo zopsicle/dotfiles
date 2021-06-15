14db916de7c3a132774649c22c685fc42f5be6c1
Chloé Kekoa <chloekek@use.startmail.com>
2021-06-15 19:06:57 +0200

    Enable syntax highlighting in Vim

--- nixos/preferences/vim.nix
+++ nixos/preferences/vim.nix
@@ -5,15 +5,19 @@
     # but it works in a terminal.
     programs.vim.defaultEditor = true;
 
-    # I really hate the Vim terminal mouse support.
+    # Install vimrc for all users (not in home directory).
     programs.vim.package = pkgs.vim_configurable.customize {
 
         # This becomes the name of the vim executable.
         name = "vim";
 
         vimrcConfig.customRC = ''
+            " I really hate the Vim terminal mouse support.
             set mouse=
             set ttymouse=
+
+            " Enable syntax highlighting.
+            syntax on
         '';
 
     };
