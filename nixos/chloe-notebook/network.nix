3b4965b9d97d7937c33f8420c9df9d9112b15f04
Chloé <chloekek@use.startmail.com>
2021-09-24 21:40:19 +0200

    Add Wi-Fi access points

--- nixos/chloe-notebook/network.nix
+++ nixos/chloe-notebook/network.nix
@@ -1,6 +1,10 @@
 { ... }:
 
 {
+    imports = [
+        /home/r/dotfiles-private/network.nix
+    ];
     networking.wireless.enable = true;
     networking.wireless.interfaces = [ "wlp2s0" ];
+    networking.wireless.userControlled.enable = true;
 }
