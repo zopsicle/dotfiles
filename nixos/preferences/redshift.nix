926903c99f11bc63ec9fc7940128f1acbb6ab892
Chloé Kekoa <chloekek@use.startmail.com>
2021-10-18 20:53:28 +0200

    Disable redshift

--- nixos/preferences/redshift.nix
+++ nixos/preferences/redshift.nix
@@ -2,7 +2,7 @@
 
 {
     # Redshift makes the screen less blue at night.
-    services.redshift.enable = true;
+    services.redshift.enable = false;
     location.latitude = 52.0843147;
     location.longitude = 4.94241;
 }
