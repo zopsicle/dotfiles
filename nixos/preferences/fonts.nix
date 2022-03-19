2bbc8d2b61551f441af5daa45400e3a5aaaa8d75
Chloé Kekoa <chloekek@use.startmail.com>
2022-03-19 01:32:32 +0100

    Install Comic Mono font family

--- nixos/preferences/fonts.nix
+++ nixos/preferences/fonts.nix
@@ -1,8 +1,21 @@
 { pkgs, ... }:
 
+let
+    comic-mono = pkgs.fetchzip {
+        name = "comic-mono";
+        url = "https://github.com/dtinth/comic-mono-font/archive/9a96d04cdd2919964169192e7d9de5012ef66de4.zip";
+        sha256 = "0x5s0wbmhn04l9sd4jb1mia2j53qid5n2r0gb7w3m5vnfr86slip";
+        postFetch = ''
+            mkdir --parents "$out/share/fonts"
+            unzip -j "$downloadedFile" '*.ttf' -d "$out/share/fonts/truetype"
+        '';
+    };
+in
+
 {
     # Globally installed fonts.
     fonts.fonts = [
+        comic-mono
         pkgs.fixedsys-excelsior
         pkgs.montserrat
         pkgs.noto-fonts-cjk
