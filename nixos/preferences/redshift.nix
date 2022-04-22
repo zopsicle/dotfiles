b51b1fff5b13a42fa166d344a4ee6bafb9a1c659
Chloé Kekoa <chloekek@use.startmail.com>
2022-04-23 01:54:09 +0200

    Make redshift more red at night

--- nixos/preferences/redshift.nix
+++ nixos/preferences/redshift.nix
@@ -3,6 +3,7 @@
 {
     # Redshift makes the screen less blue at night.
     services.redshift.enable = true;
+    services.redshift.temperature.night = 2500;
     location.latitude = 52.0843147;
     location.longitude = 4.94241;
 }
