76ba0917ac21952999f505eabd3b89f38ba30b69
Chloé Kekoa <chloekek@use.startmail.com>
2022-06-27 13:11:02 +0200

    New network interface

--- nixos/chloe-desktop/network.nix
+++ nixos/chloe-desktop/network.nix
@@ -4,7 +4,7 @@
     # I have no idea what this means.
     # Computer works with these options.
     networking.useDHCP = false;
-    networking.interfaces.enp3s0.useDHCP = true;
+    networking.interfaces.enp5s0.useDHCP = true;
 
     # This computer does not use Wi-Fi.
     networking.wireless.enable = false;
